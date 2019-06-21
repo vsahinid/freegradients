77b5aabe4643fc637730229ec69909a9cd0e5700
vsahinid <vsahinid@alumni.cmu.edu>
2019-06-20 20:33:03 -0500

    updated favicon

--- src/components/Arrows.js
+++ src/components/Arrows.js
@@ -4,8 +4,8 @@ import "./Arrows.css";
 const Arrows = () => {
   return (
     <div style={{ display: "flex" }}>
-      <button className="btn btn-primary leftButton btn-lg">{"<"}</button>
-      <button className="btn btn-primary rightButton btn-lg">{">"}</button>
+      <button className="btn btn-secondary leftButton btn-lg">{"<"}</button>
+      <button className="btn btn-secondary rightButton btn-lg">{">"}</button>
     </div>
   );
 };

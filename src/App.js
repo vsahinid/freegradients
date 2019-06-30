dff54a31d8bdd9c96e419880dc3496203662003b
vsahinid <vsahinid@alumni.cmu.edu>
2019-06-29 23:14:35 -0500

    removed comments

--- src/App.js
+++ src/App.js
@@ -3,38 +3,6 @@ import { COLORS } from "./MyColors";
 import "./App.css";
 import ColorPanel from "./components/ColorPanel/ColorPanel";
 import Navbar from "./components/Navbar/Navbar";
-// const COLORS = [
-//   {
-//     id: 1,
-//     name: "Capricious",
-//     color1: "#430089",
-//     color2: "#82ffa1",
-//     orientation1: "top",
-//     orientation2: "left",
-//     definition:
-//       "Adjective. Indicative or subject to sudden or unpredictable change; erratic."
-//   },
-//   {
-//     id: 2,
-//     name: "Ostentatious",
-//     color1: "#DAA520",
-//     color2: "#4B0082",
-//     orientation1: "top",
-//     orientation2: "right",
-//     definition:
-//       "Adjective. Characterized by or given to pretentious or conspicuous show in an attempt to impress others."
-//   },
-//   {
-//     id: 3,
-//     name: "Conspicuous",
-//     color1: "#B57B22",
-//     color2: "#DA3F2B",
-//     orientation1: "top",
-//     orientation2: "left",
-//     definition:
-//       "Adjective. Easily seen or noticed; readily visible or observable."
-//   }
-// ];
 const myColor = COLORS[0];
 
 class App extends Component {

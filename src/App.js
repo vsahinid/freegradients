46790f557de60f0017c8d91a85ee22b30ad112d3
vsahinid <vsahinid@alumni.cmu.edu>
2019-06-29 23:10:29 -0500

    Not referring to colors.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React, { Component } from "react";
-import { COLORS } from "../src/Colors";
+// import { COLORS } from "../src/Colors";
 import "./App.css";
 import ColorPanel from "./components/ColorPanel/ColorPanel";
 import Navbar from "./components/Navbar/Navbar";
-
+const COLORS = [
+  {
+    id: 1,
+    name: "Capricious",
+    color1: "#430089",
+    color2: "#82ffa1",
+    orientation1: "top",
+    orientation2: "left",
+    definition:
+      "Adjective. Indicative or subject to sudden or unpredictable change; erratic."
+  },
+  {
+    id: 2,
+    name: "Ostentatious",
+    color1: "#DAA520",
+    color2: "#4B0082",
+    orientation1: "top",
+    orientation2: "right",
+    definition:
+      "Adjective. Characterized by or given to pretentious or conspicuous show in an attempt to impress others."
+  },
+  {
+    id: 3,
+    name: "Conspicuous",
+    color1: "#B57B22",
+    color2: "#DA3F2B",
+    orientation1: "top",
+    orientation2: "left",
+    definition:
+      "Adjective. Easily seen or noticed; readily visible or observable."
+  }
+];
 const myColor = COLORS[0];
 
 class App extends Component {
@@ -19,13 +50,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Navbar
+        <Navbar
           orientation1={this.state.orientation1}
           orientation2={this.state.orientation2}
           color1={this.state.color1}
           color2={this.state.color2}
           name={this.state.name}
-        /> */}
+        />
         <ColorPanel
           orientation1={this.state.orientation1}
           orientation2={this.state.orientation2}
